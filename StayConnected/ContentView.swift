d67902c91d77c14b87aa2b01968ff0508dae238b
syblades <86990570+syblades@users.noreply.github.com>
2022-01-28 17:08:59 -0800

    Initial Commit

---
+++ StayConnected/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StayConnected
+//
+//  Created by Symone Blades on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StayConnected/StayConnectedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayConnectedApp.swift
+//  StayConnected
+//
+//  Created by Symone Blades on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct StayConnectedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
